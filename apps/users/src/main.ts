5a0502185f2d12cd074fec4b71b275e54d09cb74
Jayesh <jayesh.anandani@aravsys.com>
2021-02-26 18:45:16 +0530

    CHNAGES added

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,13 +1,16 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { UsersModule } from "./users.module";
 import { ConfigService } from "@nestjs/config";
+
+import { UsersModule } from "./users.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
   const config = app.get(ConfigService);
+
   await app.listen(config.get("app.userPort"));
   const url = await app.getUrl();
+
   Logger.log(`${url}/graphql`);
 }
 bootstrap();
